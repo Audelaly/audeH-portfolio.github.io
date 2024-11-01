82c1b952542e17c8fb4dc5914c2784c57fc1248e
aude_halipre <aude_halipre@ens.univ-artois.fr>
2024-11-01 19:17:53 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+const {TerserPlugin} = require("next/dist/build/webpack/plugins/terser-webpack-plugin/src");
+
 const nextConfig = {
     experimental: {
         optimizeServerReact: true,
@@ -19,5 +21,60 @@ const nextConfig = {
     productionBrowserSourceMaps: false,
     swcMinify: true,
     reactStrictMode: true,
+    webpack(config, { isServer, dev }) {
+        if (!isServer) {
+            config.optimization.splitChunks = {};
+        }
+
+        if (config.cache && !dev) {
+            config.cache = Object.freeze({
+                type: 'memory',
+            });
+        }
+
+        config.optimization = {
+            ...config.optimization,
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    parallel: 4,
+                    terserOptions: {
+                        compress: {
+                            drop_console: true,
+                            drop_debugger: true,
+                            pure_funcs: ['console.log'],
+                            passes: 2,
+                            toplevel: true,
+                        },
+                        mangle: true,
+                        format: {
+                            comments: false,
+                        },
+                    },
+                }),
+            ],
+            checkWasmTypes: false,
+            concatenateModules: true,
+            removeEmptyChunks: true,
+            mergeDuplicateChunks: true,
+            flagIncludedChunks: true,
+            sideEffects: true,
+            providedExports: true,
+            splitChunks: {
+                minSize: 17000,
+                minRemainingSize: 0,
+                minChunks: 1,
+                maxAsyncRequests: 30,
+                maxInitialRequests: 30,
+                automaticNameDelimiter: "_",
+                enforceSizeThreshold: 30000,
+                chunks: 'async',
+            },
+        };
+
+        config.resolve.extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+        return config;
+    },
 };
 export default nextConfig;
