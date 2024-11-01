55e8dd4092c623e9fb7233e6278b0f52deed971b
aude_halipre <aude_halipre@ens.univ-artois.fr>
2024-11-01 19:03:43 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,3 @@
-const {TerserPlugin} = require("next/dist/build/webpack/plugins/terser-webpack-plugin/src");
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-});
-
-/**
- * @type {import('next').NextConfig}
- */
 const nextConfig = {
     experimental: {
         optimizeServerReact: true,
@@ -28,43 +20,5 @@ const nextConfig = {
     productionBrowserSourceMaps: false,
     swcMinify: true,
     reactStrictMode: true,
-    webpack(config, { isServer, dev }) {
-        if (!isServer) {
-            config.optimization.splitChunks = {};
-        }
-
-        if (config.cache && !dev) {
-            config.cache = Object.freeze({
-                type: 'memory',
-            });
-        }
-
-        config.optimization = {
-            ...config.optimization,
-            minimize: true,
-            checkWasmTypes: false,
-            concatenateModules: true,
-            removeEmptyChunks: true,
-            mergeDuplicateChunks: true,
-            flagIncludedChunks: true,
-            sideEffects: true,
-            providedExports: true,
-            splitChunks: {
-                minSize: 17000,
-                minRemainingSize: 0,
-                minChunks: 1,
-                maxAsyncRequests: 30,
-                maxInitialRequests: 30,
-                automaticNameDelimiter: "_",
-                enforceSizeThreshold: 30000,
-                chunks: 'async',
-            },
-        };
-
-        config.resolve.extensions = ['.js', '.jsx', '.ts', '.tsx'];
-
-        return config;
-    },
 };
-
-module.exports = withBundleAnalyzer(nextConfig);
+export default nextConfig;
