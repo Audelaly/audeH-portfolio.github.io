6e01a30086c61d92713102e34f3322ed873c46f1
aude_halipre <aude_halipre@ens.univ-artois.fr>
2024-11-01 19:27:57 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,80 +1,9 @@
-import TerserPlugin from "next/dist/build/webpack/plugins/terser-webpack-plugin/src/index.js";
+/**
+ * @type {import('next').NextConfig}
+ */
 
 const nextConfig = {
-    experimental: {
-        optimizeServerReact: true,
-        optimisticClientCache: true,
-        serverMinification: true,
-        scrollRestoration: true,
-        serverSourceMaps: false,
-        workerThreads: true,
-    },
-    compiler: {
-        styledComponents: true,
-        reactRemoveProperties: true,
-        removeConsole: true,
-    },
-    typescript: { ignoreBuildErrors: true },
-    eslint: { ignoreDuringBuilds: true },
-    compress: true,
-    poweredByHeader: false,
-    productionBrowserSourceMaps: false,
-    swcMinify: true,
-    reactStrictMode: true,
-    webpack(config, { isServer, dev }) {
-        if (!isServer) {
-            config.optimization.splitChunks = {};
-        }
-
-        if (config.cache && !dev) {
-            config.cache = Object.freeze({
-                type: 'memory',
-            });
-        }
-
-        config.optimization = {
-            ...config.optimization,
-            minimize: true,
-            minimizer: [
-                new TerserPlugin({
-                    parallel: 4,
-                    terserOptions: {
-                        compress: {
-                            drop_console: true,
-                            drop_debugger: true,
-                            pure_funcs: ['console.log'],
-                            passes: 2,
-                            toplevel: true,
-                        },
-                        mangle: true,
-                        format: {
-                            comments: false,
-                        },
-                    },
-                }),
-            ],
-            checkWasmTypes: false,
-            concatenateModules: true,
-            removeEmptyChunks: true,
-            mergeDuplicateChunks: true,
-            flagIncludedChunks: true,
-            sideEffects: true,
-            providedExports: true,
-            splitChunks: {
-                minSize: 17000,
-                minRemainingSize: 0,
-                minChunks: 1,
-                maxAsyncRequests: 30,
-                maxInitialRequests: 30,
-                automaticNameDelimiter: "_",
-                enforceSizeThreshold: 30000,
-                chunks: 'async',
-            },
-        };
-
-        config.resolve.extensions = ['.js', '.jsx', '.ts', '.tsx'];
-
-        return config;
-    },
+    //output: 'export',
 };
+
 export default nextConfig;
