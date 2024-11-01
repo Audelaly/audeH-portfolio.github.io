d7d366f649d9216c44c2a376967a913858f21810
aude_halipre <aude_halipre@ens.univ-artois.fr>
2024-11-01 18:47:33 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,20 @@
-/**
- * @type {import('next').NextConfig}
- */
+// next.config.js
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+const withCompression = require('next-compression');
 
-const nextConfig = {
-    //output: 'export',
-};
-
-export default nextConfig;
+module.exports = withBundleAnalyzer(
+    withCompression({
+        images: {
+            domains: ['example.com'], // Add your image domains here
+        },
+        compress: true,
+        webpack(config, { isServer }) {
+            if (!isServer) {
+                config.resolve.alias['@sentry/node'] = '@sentry/browser';
+            }
+            return config;
+        },
+    })
+);
