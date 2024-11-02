e818c18a056364675093a93f51367e9e6702e752
aude_halipre <aude_halipre@ens.univ-artois.fr>
2024-11-02 12:36:06 +0100

    update

--- app/ui/skills-section.tsx
+++ app/ui/skills-section.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 const SkillsSection = () => {
     return (
@@ -14,26 +15,26 @@ const SkillsSection = () => {
 
                     <div className="col-lg-3" data-aos="fade-up">
                         <div className="progress">
-                            <img src="/audeH-portfolio.github.io/img/java.webp" alt="Java" />
-                            <img src="/audeH-portfolio.github.io/img/python.webp" alt="Python" />
-                            <img src="/audeH-portfolio.github.io/img/linux.webp" alt="Linux" />
+                            <Image src="/audeH-portfolio.github.io/img/java.webp" alt="Java" width={100} height={100} />
+                            <Image src="/audeH-portfolio.github.io/img/python.webp" alt="Python" width={100} height={100} />
+                            <Image src="/audeH-portfolio.github.io/img/linux.webp" alt="Linux" width={100} height={100} />
                         </div>
                     </div>
 
                     <div className="col-lg-3" data-aos="fade-up" data-aos-delay="100">
                         <div className="progress">
-                            <img src="/audeH-portfolio.github.io/img/flask-horizontal.webp" alt="Flask" />
-                            <img src="/audeH-portfolio.github.io/img/laravel_11zon.webp" alt="Laravel" />
-                            <img src="/audeH-portfolio.github.io/img/next-js.svg" alt="Next JS"/>
+                            <Image src="/audeH-portfolio.github.io/img/flask-horizontal.webp" alt="Flask" width={100} height={100} />
+                            <Image src="/audeH-portfolio.github.io/img/laravel_11zon.webp" alt="Laravel" width={100} height={100} />
+                            <Image src="/audeH-portfolio.github.io/img/next-js.svg" alt="Next JS" width={100} height={100} />
                         </div>
                     </div>
 
                     <div className="col-lg-3" data-aos="fade-up">
                         <div className="progress">
-                            <img src="/audeH-portfolio.github.io/img/html.svg" alt="HTML5" />
-                            <img src="/audeH-portfolio.github.io/img/css3.svg" alt="CSS3" />
-                            <img src="/audeH-portfolio.github.io/img/javascript.webp" alt="JavaScript" />
-                            <img src="/audeH-portfolio.github.io/img/PHP-logo.webp" alt="PHP" />
+                            <Image src="/audeH-portfolio.github.io/img/html.svg" alt="HTML5" width={100} height={100} />
+                            <Image src="/audeH-portfolio.github.io/img/css3.svg" alt="CSS3" width={100} height={100} />
+                            <Image src="/audeH-portfolio.github.io/img/javascript.webp" alt="JavaScript" width={100} height={100} />
+                            <Image src="/audeH-portfolio.github.io/img/PHP-logo.webp" alt="PHP" width={100} height={100} />
                         </div>
                     </div>
 
